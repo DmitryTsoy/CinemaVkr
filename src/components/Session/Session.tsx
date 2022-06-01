b714f47fcfd02c926c703b07cb6756fb26b9497e
Nikita Firsov <nfirsov513@gmail.com>
2022-06-01 22:17:50 +0300

    add static sesson places

--- src/components/Session/Session.tsx
+++ src/components/Session/Session.tsx
@@ -24,8 +24,78 @@ export default function Session() {
 
                 <div className="oneSession__places-container">
 
-                    <div className="oneSession__array">
+                    <div className="oneSession__places-container-wrapper">
+                        <div className="oneSession__array">
 
+                            <div className="oneSession__array-row">
+                                <div className="oneSession__array-rows">
+                                    <p>1</p>
+                                    <p>2</p>
+                                    <p>3</p>
+                                    <p>4</p>
+                                    <p>5</p>
+                                </div>
+                            </div>
+
+                            <div className="oneSession__array-screen">
+                                <div></div>
+                            </div>
+
+                            <div className="oneSession__array-places">
+
+                                <div className="oneSession__array-line">
+                                    <div className="oneSession__array-element oneSession__array-element--busy"></div>
+                                    <div className="oneSession__array-element oneSession__array-element--free">2</div>
+                                    <div className="oneSession__array-element oneSession__array-element--active">3</div>
+                                    <div className="oneSession__array-element oneSession__array-element--active">4</div>
+                                    <div className="oneSession__array-element oneSession__array-element--free">5</div>
+                                    <div className="oneSession__array-element oneSession__array-element--free">6</div>
+                                </div>
+
+
+
+
+                                <div className="oneSession__array-line">
+                                    <div className="oneSession__array-element oneSession__array-element--busy"></div>
+                                    <div className="oneSession__array-element oneSession__array-element--free">2</div>
+                                    <div className="oneSession__array-element oneSession__array-element--active">3</div>
+                                    <div className="oneSession__array-element oneSession__array-element--active">4</div>
+                                    <div className="oneSession__array-element oneSession__array-element--free">5</div>
+                                    <div className="oneSession__array-element oneSession__array-element--free">6</div>
+                                </div>
+
+                                <div className="oneSession__array-line">
+                                    <div className="oneSession__array-element oneSession__array-element--busy"></div>
+                                    <div className="oneSession__array-element oneSession__array-element--free">2</div>
+                                    <div className="oneSession__array-element oneSession__array-element--active">3</div>
+                                    <div className="oneSession__array-element oneSession__array-element--active">4</div>
+                                    <div className="oneSession__array-element oneSession__array-element--free">5</div>
+                                    <div className="oneSession__array-element oneSession__array-element--free">6</div>
+                                </div>
+
+                                <div className="oneSession__array-line">
+                                    <div className="oneSession__array-element oneSession__array-element--busy"></div>
+                                    <div className="oneSession__array-element oneSession__array-element--free">2</div>
+                                    <div className="oneSession__array-element oneSession__array-element--active">3</div>
+                                    <div className="oneSession__array-element oneSession__array-element--active">4</div>
+                                    <div className="oneSession__array-element oneSession__array-element--free">5</div>
+                                    <div className="oneSession__array-element oneSession__array-element--free">6</div>
+                                </div>
+
+                                <div className="oneSession__array-line">
+                                    <div className="oneSession__array-element oneSession__array-element--busy"></div>
+                                    <div className="oneSession__array-element oneSession__array-element--free">2</div>
+                                    <div className="oneSession__array-element oneSession__array-element--active">3</div>
+                                    <div className="oneSession__array-element oneSession__array-element--active">4</div>
+                                    <div className="oneSession__array-element oneSession__array-element--free">5</div>
+                                    <div className="oneSession__array-element oneSession__array-element--free">6</div>
+                                </div>
+
+
+
+
+                            </div>
+                        </div>
                     </div>
                     <BuyButton>Купить билет</BuyButton>
                 </div>
